d609113c2e5a9e04c037efd8e18d9e0d990df33b
Shashwat Joshi <shashwat2002joshi@gmail.com>
2023-02-09 14:59:55 +0530

    first commit

---
+++ src/main/java/wissen/insurancedomainapp/InsuranceDomainAppApplication.java
@@ -0,0 +1,13 @@
+package wissen.insurancedomainapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceDomainAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsuranceDomainAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wissen/insurancedomainapp/InsuranceDomainAppApplicationTests.java
@@ -0,0 +1,13 @@
+package wissen.insurancedomainapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceDomainAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
